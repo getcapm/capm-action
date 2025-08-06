fe7c7049721dafaf7d15d2f374d4916e53adf7d5
Rob van der Leek <5324924+robvanderleek@users.noreply.github.com>
2025-08-06 13:41:23 +0200

    fix: 🐛 Gracefully handle non-zero return code

--- src/action.ts
+++ src/action.ts
@@ -3,7 +3,7 @@ import {getInput} from "@actions/core";
 import {exec} from "@actions/exec";
 import {downloadCapmBinary} from "./capm";
 import {version} from "./version";
-import signale, {info, success} from "signale";
+import signale, {fatal, info, success} from "signale";
 
 signale.config({
     displayTimestamp: true
@@ -18,8 +18,12 @@ async function main() {
     await exec(capmBinary, ['--version']);
     info('Running CAPM...');
     const exitCode = await exec(capmBinary, ['run'], {ignoreReturnCode: true});
+    if (exitCode === 0) {
+        success('Done!');
+    } else {
+        fatal(`CAPM exited with code ${exitCode}`);
+    }
     fs.unlinkSync(capmBinary);
-    success('Done!');
     process.exit(exitCode);
 }
 
